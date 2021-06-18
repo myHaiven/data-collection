a30054e0a94e4094558ea28fb392e3b627c1a148
jql6 <jql6@sfu.ca>
2021-06-17 21:27:10 -0700

    Test adding end line

--- silence-removal/vad.py
+++ silence-removal/vad.py
@@ -168,3 +168,4 @@ voice_samples = vad.get_voice_samples()
 output_name = sys.argv[2]
 # Write the processed audio into a new `.wav` file
 wf.write(f"{output_name}.wav", sample_rate, voice_samples)
+
