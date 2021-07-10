8eb0e8bb27f53f752792bf93bc0541396adb4297
jql6 <jql6@sfu.ca>
2021-07-09 22:43:51 -0700

    Remove rename audio file path and format code

--- silence-removal/convert_to_wav.py
+++ silence-removal/convert_to_wav.py
@@ -3,17 +3,17 @@
 from datetime import datetime
 from useful_functions import set_up_logging
 from pathlib import Path
-from pydub import AudioSegment # This also requires ffmpeg on the device
+from pydub import AudioSegment  # This also requires ffmpeg on the device
 import re
 import logging
 
 # Function that converts `.aac` or `.mp3` files to `.wav`
-def convert_to_wav(audio_filename):
+def convert_to_wav(audio_file_path):
     """
     Function that converts a `.aac` audio file into a `.wav` audio file.
     
     Arguments:
-    audio_filename | str
+    audio_file_path | str
     The filename of the `.aac` audio file you want to convert into `.wav`.
     
     Returns:
@@ -21,25 +21,28 @@ def convert_to_wav(audio_filename):
     The filename of the `.wav` file.
     """
     # Set up the logging
-    set_up_logging(log_path = "logging/convert_to_wav/",
-                   log_level = logging.INFO)
-    logging.info(f"Audio filename: {audio_filename}")
+    set_up_logging(log_path="logging/convert_to_wav/", log_level=logging.INFO)
+    logging.info(f"Audio filename: {audio_file_path.name}")
     # Audio extension that will be converted to `.wav`
+    audio_file_extension = audio_file_path.suffix
     audio_extensions = [".aac", ".mp3"]
-    audio_file_extension = re.search(pattern = "\.\w+$",
-                                     string = audio_filename).group(0)
-    
     if audio_file_extension in audio_extensions:
         # Load the audio file
-        sound = AudioSegment.from_file(audio_filename)
-        # Convert filename extension from to `.wav`
-        new_filename = re.sub(pattern = f"{audio_file_extension}$",
-                              repl = ".wav",
-                              string = audio_filename)
+        sound = AudioSegment.from_file(audio_file_path)
+        # Convert filename extension to `.wav`
+        new_filename = re.sub(
+            pattern=f"{audio_file_extension}$",
+            repl=".wav",
+            string=str(audio_file_path.name),
+        )
         logging.info(f"New audio filename: {new_filename}")
-        sound.export(new_filename, format = "wav")
+        print(new_filename)
+        sound.export(
+            Path(Path(audio_file_path).parent, new_filename), format="wav"
+        )
     else:
-        raise Exception(f"The '{audio_file_extension}' " +
-                        "extension is not supported!")
-    
+        raise Exception(
+            f"The '{audio_file_extension}' " + "extension is not supported!"
+        )
+
     return new_filename
