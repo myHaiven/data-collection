c075780d7f1b1f74fc4b384de8046a29a875c41c
jql6 <jql6@sfu.ca>
2021-07-10 20:10:17 -0700

    Add segmentation script

---
+++ segmentation/segmentation.py
@@ -0,0 +1,38 @@
+# segmentation.py
+
+from pathlib import Path  # For writing videos into the data folder
+from pydub import AudioSegment  # This also requires ffmpeg on the device
+import sys
+
+# Path to the file; can loop through a date folder later on
+file_path = Path(
+    Path.cwd().parent,
+    "data",
+    "reddit",
+    "2021-07-10",
+    "q356rndwpj251",
+    "q356rndwpj251_raw.aac",
+)
+
+date_path = Path(Path.cwd().parent, "data", "reddit", sys.argv[1])
+
+# Pydub uses milleseconds as the units; 10 000 is 10 seconds
+ten_seconds = 10 * 1000
+
+date_path.glob("[!.]*")
+
+# Loop through folders in date folder, excluding hidden files
+for folder in date_path.glob("[!.]*"):
+    file_path = Path(next(Path(folder).glob("*_processed.wav")))
+    # Import the file
+    audio_file = AudioSegment.from_file(file_path)
+    # Determine the number of segments to loop through
+    number_of_segments = round(audio_file.duration_seconds / 10)
+    # For each segment of at most 10 seconds, export the segment
+    for i in range(number_of_segments):
+        current_segment = audio_file[i * ten_seconds : (i + 1) * ten_seconds]
+        current_segment.export(
+            Path(file_path.parent, f"{file_path.stem}_{i}.wav"), format="wav"
+        )
+        print(f"{file_path.stem}_{i}.wav")
+
